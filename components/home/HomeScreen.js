0fd4cdf89d7f68b6a761134960317294e511ff30
Prince04u <2401030630043@silveroakuni.ac.in>
2026-08-06 15:01:22 +0530

    Update home page products to match apex-king

--- components/home/HomeScreen.js
+++ components/home/HomeScreen.js
@@ -6,52 +6,100 @@ import BottomNav from "./BottomNav";
 
 const PRODUCTS = [
   {
-    id: 1,
-    title: "【 Now 】 Series White 18K Gold Pair Ring",
-    price: "38570.00",
-    image: "https://art.apex-king.com/uploads/images/51iEBQzCL5L._UL1500_.jpg"
+    "id": 1,
+    "title": "Joyalukkas 18k (750) Rose Gold and Solitaire Pendant for Girls",
+    "price": "38576.00",
+    "image": "https://art.apex-king.com/uploads/images/51iEBQzCL5L._UL1500_.jpg"
   },
   {
-    id: 2,
-    title: "【Cute Pet】 Series Rose 18K Gold Pearl Pendant",
-    price: "2899.00",
-    image: "https://art.apex-king.com/uploads/images/71JvL64Y3cL._UY695_.jpg"
+    "id": 2,
+    "title": "Ratnavali Jewels American Diamond Traditional Fashion Jewellery Green Necklace Pendant Set with Earring for Women/Girls RV2916G",
+    "price": "2899.00",
+    "image": "https://art.apex-king.com/uploads/images/71JvL64Y3cL._UY695_.jpg"
   },
   {
-    id: 3,
-    title: "18K Gold Diamond Ruby Deer Horn Collar Chain",
-    price: "4559.00",
-    image: "https://art.apex-king.com/uploads/images/71YWzTc2omL._UY695_.jpg"
+    "id": 3,
+    "title": "Swasti Jewels Heart Shape Fashion Jewellery Set Pendant Earrings for Women",
+    "price": "4559.00",
+    "image": "https://art.apex-king.com/uploads/images/71YWzTc2omL._UY695_.jpg"
   },
   {
-    id: 4,
-    title: "Enzo Jewelry 14k Gold Seven Colored Gemstone Pendant",
-    price: "5599.00",
-    image: "https://art.apex-king.com/uploads/images/4.jpg"
+    "id": 4,
+    "title": "Om Jewells Rhodium Plated Blue Crystal Jewellery Combo of Designer Drop Pendant Set with Adjustable Bangle Kada and Adjustable Solitaire Finger Ring for Girls and Women CO1000209",
+    "price": "1599.00",
+    "image": "https://art.apex-king.com/uploads/images/4.jpg"
   },
   {
-    id: 5,
-    title: "Sukkhi Gleaming Pearl Gold Plated Wedding Jewellery Kundan Peacock Meenakari Multi-String Necklace",
-    price: "1745.00",
-    image: "https://art.apex-king.com/uploads/images/5.jpg"
+    "id": 5,
+    "title": "Sukkhi Gleaming Pearl Gold Plated Wedding Jewellery Kundan Peacock Meenakari Multi-String Necklace Set for Women (2191NGLDPP1560)",
+    "price": "1745.00",
+    "image": "https://art.apex-king.com/uploads/images/5.jpg"
   },
   {
-    id: 6,
-    title: "Ananth Jewels 925 Sterling Silver BIS Hallmarked Heart Bracelet for Women",
-    price: "9000.00",
-    image: "https://art.apex-king.com/uploads/images/6.jpg"
+    "id": 6,
+    "title": "Ananth Jewels 925 Sterling Silver BIS Hallmarked Heart Bracelet for Women",
+    "price": "9000.00",
+    "image": "https://art.apex-king.com/uploads/images/6.jpg"
   },
   {
-    id: 7,
-    title: "Handicraft Kottage ® 1gm 22Ct Gold Plated Pendant and Chain for Men/Women/Girls",
-    price: "999.00",
-    image: "https://art.apex-king.com/uploads/images/7.jpg"
+    "id": 7,
+    "title": "Handicraft Kottage ® 1gm 22Ct Gold Plated Pendant and Chain for Men/Women/Girls",
+    "price": "999.00",
+    "image": "https://art.apex-king.com/uploads/images/7.jpg"
   },
   {
-    id: 8,
-    title: "Mansiyaorange Combo of Two Party One Gram Gold Forming Long Haram and Choker Set",
-    price: "3199.00",
-    image: "https://art.apex-king.com/uploads/images/8.jpg"
+    "id": 8,
+    "title": "Mansiyaorange Combo of Two Party One Gram Gold Forming Long Haram and Choker Multi Color Jewellery Necklace/Juelry/jwelry Set Jewellery for Women",
+    "price": "3199.00",
+    "image": "https://art.apex-king.com/uploads/images/8.jpg"
+  },
+  {
+    "id": 9,
+    "title": "Young & Forever Fashion Jewellery Elite Rose Gold Plated Geometric Shape Stud Earring Pendant Set for Women Princess Length Delicate Chain Cubic Zirconia Necklace Set for Girls Jewelry",
+    "price": "4450.00",
+    "image": "https://art.apex-king.com/uploads/images/9.jpg"
+  },
+  {
+    "id": 10,
+    "title": "Ratnavali Jewels American Diamond Cz Gold Plated Necklace Set Tennis Necklace Single Line Solitaire Set With Chain & Earring For Women",
+    "price": "4000.00",
+    "image": "https://art.apex-king.com/uploads/images/10.jpg"
+  },
+  {
+    "id": 11,
+    "title": "chandrika pearls gems & jewellers Sania Mirza Style Without Piercing Clip on Pressing Type Nose Ring for Women & Girls",
+    "price": "278.00",
+    "image": "https://art.apex-king.com/uploads/images/11.jpg"
+  },
+  {
+    "id": 12,
+    "title": "Chandrika Pearls Gems & Jewellers Sterling Silver Pendant Earring with Swarovski Crystal for Girls",
+    "price": "1060.00",
+    "image": "https://art.apex-king.com/uploads/images/12.jpg"
+  },
+  {
+    "id": 13,
+    "title": "Chandrika Pearls Gems & Jewellers 92.5 Sterling Silver Real Diamond Valentine Crown King Queen Ring for Girls & Women",
+    "price": "1920.00",
+    "image": "https://art.apex-king.com/uploads/images/13.jpg"
+  },
+  {
+    "id": 14,
+    "title": "Chandrika Pearls Gems & Jewellers 925 Pure Sterling Silver Letters Initial Pendant with Gold Polish (R)",
+    "price": "476.00",
+    "image": "https://art.apex-king.com/uploads/images/14.jpg"
+  },
+  {
+    "id": 15,
+    "title": "Chandrika Pearls Gems & Jewellers Dhanteras Brass Hindu Puja Camphor Burner Lamp Panch Aarti - 5 Face For Puja",
+    "price": "1880.00",
+    "image": "https://art.apex-king.com/uploads/images/15.jpg"
+  },
+  {
+    "id": 16,
+    "title": "Jewels Galaxy Designer American Diamond Gold Plated Bangles for Women/Girls",
+    "price": "1999.00",
+    "image": "https://art.apex-king.com/uploads/images/16.jpg"
   }
 ];
 
